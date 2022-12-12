31b8662e9bf31351867245ad0106db21e0919992
Henry Bridge <hbridge@oldfashionedsw.com>
2022-12-12 16:11:57 -0500

    add day 2 and make loading less fancy

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,59 @@
 import { argv } from "process";
 
-const glob = require('glob');
-const days = [];
+const dayNum = process.argv[2] ? Number(process.argv[2]) : 0;
+const fs = require('fs');
 
-const mode = process.argv[2] || 'all';
+const days = {
+    0: () => 0, 
+    1: function (prompt: string) {
+        const elfChunks = prompt.split('\n\n');
 
-glob.sync(__dirname + '/days/*.js').forEach(
-    file => days.push(require(file))
-);
+        let maxFound = 0;
+        for (const chunk of elfChunks) {
+            const numbers = chunk.split("\n").map(str => Number(str));
+            const elfCalories = numbers.reduce((prev, cur) => prev + cur, 0);
+            maxFound = Math.max(elfCalories, maxFound);
+        }
 
-if (mode == 'last') {
-    days[days.length - 1].run();
+        return maxFound;
+    },
+    2: function (prompt: string) {
+        const elfChunks = prompt.split('\n\n');
+
+        let topThreeAsc = [0, 0, 0];
+        for (const chunk of elfChunks) {
+            const numbers = chunk.split("\n").map(str => Number(str));
+            const elfCalories = numbers.reduce((prev, cur) => prev + cur, 0);
+            
+            let insertIndex = -1;
+            let i = 0;
+            while(elfCalories > topThreeAsc[i]) {
+                insertIndex = i;
+                i++;
+            }
+
+            if (insertIndex >= 0) {
+                //insert the new elf value after the one it's bigger than
+                topThreeAsc.splice(insertIndex + 1, 0, elfCalories);
+                topThreeAsc.splice(0, 1); // discard the lowest
+            }
+        }
+
+        return topThreeAsc.reduce((prev, cur) => prev + cur);
+    }
 }
 
-for (let day of days) {
-    day.run();
+function run (dayNum = Object.keys(days).length - 1) {
+    console.log('\n*** Running day %d***', dayNum);
+    const example = fs.readFileSync(`${process.cwd()}/data/day${dayNum}example.txt`);
+    const input = fs.readFileSync(`${process.cwd()}/data/day${dayNum}data.txt`);
+
+    console.log("Example result: %s", JSON.stringify(days[dayNum](example.toString())));
+    console.log("Input result: %s\n", JSON.stringify(days[dayNum](input.toString())));
 }
 
+if (dayNum > 0) {
+    run(dayNum);
+} else {
+    run();
+}
