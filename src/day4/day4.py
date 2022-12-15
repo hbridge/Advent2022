afcc54f425d19e617cb208e601a7379c3b2ff055
Henry Bridge <hbridge@oldfashionedsw.com>
2022-12-15 11:18:48 -0500

    Day 4

---
+++ src/day4/day4.py
@@ -0,0 +1,58 @@
+import pathlib
+import re
+
+script_directory = pathlib.Path(__file__).parent.resolve()
+
+example_file = open(f"{script_directory}/example.txt")
+input_file = open(f"{script_directory}/input.txt")
+
+EXAMPLE = example_file.read();
+INPUT = input_file.read();
+
+example_file.close();
+input_file.close();
+
+######## MODIFY AFTER HERE ########
+
+def char_to_pri(char):
+  val = ord(char)
+  if (ord(char) < ord('a')):
+    # it's a capital
+    return ord(char) - ord('A') + 27
+  else:
+    return ord(char) - ord('a') + 1
+
+
+
+inputs = {
+  "example": EXAMPLE.split("\n"),
+  "input": INPUT.split("\n")
+}
+
+part1_answers = {}
+part2_answers = {}
+
+regex = re.compile("(\\d+)-(\\d+),(\\d+)-(\\d+)")
+
+for input_type, lines in inputs.items():
+  answer1 = 0
+  answer2 = 0
+  for i, line in enumerate(lines):
+    a_start, a_end, b_start, b_end = [int(s) for s in regex.match(line).groups()]
+    if (a_start <= b_start and a_end >= b_end) \
+      or (b_start <= a_start and b_end >= a_end):
+      answer1 += 1
+
+    if (a_start <= b_start and a_end >= b_start) \
+      or (b_start <= a_start and b_end >= a_start):
+      answer2 += 1
+
+  part1_answers[input_type] = answer1
+  part2_answers[input_type] = answer2
+
+print(f'Part 1 Answers:')
+print(f'  Example: {part1_answers["example"]}')
+print(f'  Input: {part1_answers["input"]}')
+print(f'Part 2 Answers:')
+print(f'  Example: {part2_answers["example"]}')
+print(f'  Input: {part2_answers["input"]}')
